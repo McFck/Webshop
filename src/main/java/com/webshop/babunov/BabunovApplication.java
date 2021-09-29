c771fc6ea83f70f7bd4df09414a6d0ca9d3fef5c
McFck <ilya.sir@yandex.ru>
2021-09-29 21:36:51 +0400

    Init commit

---
+++ src/main/java/com/webshop/babunov/BabunovApplication.java
@@ -0,0 +1,13 @@
+package com.webshop.babunov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabunovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabunovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webshop/babunov/BabunovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshop.babunov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabunovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
